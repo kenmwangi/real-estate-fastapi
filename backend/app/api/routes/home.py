bee25e83f37a15818612539d9e5578ccf73d906b
kenmwangi <kenmwangi071@gmail.com>
2025-06-11 19:14:59 +0300

    feat: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
 
-router = APIRouter()
+logger = get_logger()
+
+router = APIRouter(prefix="/home")
 
 
 @router.get("/")
 def home():
+    logger.info("Home page accessed")
+    logger.debug("Home page accessed")
+    logger.error("Home page accessed")
+    logger.warning("Home page accessed")
+    logger.critical("Home page accessed")
     return {"message": "Welcome to the ARE real estate API"}
---
+++ backend/app/core/logging.py
@@ -0,0 +1,43 @@
+import os
+from loguru import logger
+from app.core.config import settings
+
+
+logger.remove()
+
+LOG_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), "logs"
+)  # put backend/app/logs
+
+LOG_FORMAT = (
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} | "
+    "{level: <48} | "
+    "{name}:{function}:{line} - "
+    "{message}"
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "debug.log"),
+    format=LOG_FORMAT,
+    level="DEBUG" if settings.Environment == "local" else "INFO",
+    filter=lambda record: record["level"].no <= logger.level("WARNING").no,
+    rotation="10MB",
+    retention="30 days",
+    compression="zip",
+)
+
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "error.log"),
+    format=LOG_FORMAT,
+    level="ERROR",
+    rotation="10MB",
+    retention="30 days",
+    compression="zip",
+    backtrace=True,
+    diagnose=True,
+)
+
+
+def get_logger():
+    return logger
